5f40b11d465baf2066c08867c1383b98a57435af
Apoorv-16 <55321478+Apoorv-16@users.noreply.github.com>
2021-03-11 01:24:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+import * as THREE from '../../libs/three/three.module.js';
+import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+
+class App{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        this.camera = new Three.PerspectiveCamera(600, window.innerWidth / window.innerHeight ,0.1,100);
+        this.camera.position.set(0,0,4);
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0xaaaaaa);
+        this.renderer = new THREE.WebglRenderer( {antialias: true});
+        this.renderer.setPixleRatio( window.devicePixelRatio);
+        this.renderer.setSize( window.innerWidth, window.innerHeight);
+        container.appendChild( this.renderer.domElement );
+        this.renderer.setAnimationLoop( this.render.bind(this));
+        
+        window.addEventListener('resize', this.resize.bind(this) );
+	}	
+    
+    resize(){
+        
+    }
+    
+	render( ) {  
+        this.renderer.render( this.scene, this.camera);
+        
+    }
+}
+
+export { App };
